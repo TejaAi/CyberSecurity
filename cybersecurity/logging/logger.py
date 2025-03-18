4b128476b28f746fc7e591c622adeb04f514d7b3
tejaai <thejaswarup1996@gmail.com>
2025-03-18 06:07:33 +0530

    updated the logging file code

--- cybersecurity/logging/logger.py
+++ cybersecurity/logging/logger.py
@@ -4,10 +4,12 @@ from datetime import datetime
 
 
 log_file_name = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path = os.path.join(os.getcwd(),"logs",log_file_name)
+log_path = os.path.join(os.getcwd(),"logs")
 os.makedirs(log_path,exist_ok=True)
+log_path = os.path.join(log_path,log_file_name)
 
 logging.basicConfig(
     filename=log_path,
-    format='[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s'
+    format='[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
